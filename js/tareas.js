9062aebc50ccf9268efc5da028c60db66625c2f1
javierdarioruizm <javierdarioruizm@gmail.com>
2021-02-04 21:53:25 +0100

    Array con tareas añadido

---
+++ js/tareas.js
@@ -0,0 +1,44 @@
+////////////// Array con las tareas iniciales //////////////
+
+
+var listaTareas = new Array();
+
+listaTareas = [
+    {
+        idTarea: 1,
+        titulo: 'EstudiarJavascript',
+        prioridad: 'urgente'
+    },
+    {
+        idTarea: 2,
+        titulo: 'Sacar a pasear al perro',
+        prioridad: 'diaria'
+    },
+    {
+        idTarea: 3,
+        titulo: 'Ir a la peluquería',
+        prioridad: 'mensual'
+
+    },
+    {
+        idTarea: 4,
+        titulo: 'Hacer ejercicio',
+        prioridad: 'diaria'
+    },
+    {
+        idTarea: 5,
+        titulo: 'Llamar a mi madre',
+        prioridad: 'semanal'
+    },
+    {
+        idTarea: 6,
+        titulo: 'Hacer la compra',
+        prioridad: 'mensual'
+    },
+    {
+        idTarea: 7,
+        titulo: 'Limpiar la casa',
+        prioridad: 'semanal'
+    }
+
+]
